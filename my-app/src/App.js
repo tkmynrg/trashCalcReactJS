79a2c0c7dc3a29b47f8539abb84d0549bce8d84f
tkmynrg <dlsenok@mail.ru>
2023-04-20 18:49:40 +0300

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,87 @@
+import {Component, useState, useEffect} from 'react';
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom/client';
 
 
+// const App = (props) => {
+//     const [counter, setCounter] = React.useState(props.counter);
+//
+//     const incCounter = () => {
+//         if (counter < 50) {
+//             setCounter(counter => counter + 1)
+//         }
+//
+//         if (counter === 50) {
+//             alert('Максимальное значение 50');
+//             resetCounter();
+//         }
+//     }
+//
+//     const decCounter = () => {
+//         if (counter > -50) {
+//             setCounter(counter => counter - 1)
+//         }
+//         if (counter === -50) {
+//             alert('Максимальное значение -50');
+//             resetCounter();
+//         }
+//     }
+//
+//     const rndCounter = () => {
+//         setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
+//     }
+//
+//     const resetCounter = () => {
+//         setCounter(props.counter)
+//     }
+//
+//
+//     return (
+//         <div className="app">
+//             <div className="counter">{counter}</div>
+//             <div className="controls">
+//                 <button onClick={incCounter}>INC</button>
+//                 <button onClick={decCounter}>DEC</button>
+//                 <button onClick={rndCounter}>RND</button>
+//                 <button onClick={resetCounter}>RESET</button>
+//             </div>
+//         </div>
+//     )
+// }
+
 const App = (props) => {
-    const [counter, setCounter] = React.useState(props.counter);
+    const _apiBase = 'https://www.cbr-xml-daily.ru/daily_json.js';
 
-    const incCounter = () => {
-        if (counter < 50) {
-            setCounter(counter => counter + 1)
+    async function getSource(url) {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
-        if (counter === 50) {
-            alert('Максимальное значение 50');
-            resetCounter();
-        }
+        return await res.json();
     }
 
-    const decCounter = () => {
-        if (counter > -50) {
-            setCounter(counter => counter - 1)
-        }
-        if (counter === -50) {
-            alert('Максимальное значение -50');
-            resetCounter();
-        }
+    async function getAllExchangesRates() {
+        const res = await getSource({_apiBase});
+        return res;
     }
 
-    const rndCounter = () => {
-        setCounter(+(Math.random() * (50 - -50) + -50).toFixed(0))
-    }
 
-    const resetCounter = () => {
-        setCounter(props.counter)
-    }
+    useEffect(() => {
+        console.log('kek')
+    });
 
     return (
         <div className="app">
-            <div className="counter">{counter}</div>
+            input
+            <div className="counter"></div>
             <div className="controls">
-                <button onClick={incCounter}>INC</button>
-                <button onClick={decCounter}>DEC</button>
-                <button onClick={rndCounter}>RND</button>
-                <button onClick={resetCounter}>RESET</button>
+                <button >INC</button>
+                <button >DEC</button>
+                <button >RND</button>
+                <button >RESET</button>
             </div>
         </div>
     )
@@ -50,7 +89,7 @@ const App = (props) => {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App counter={0}/>
+    <App props={0}/>
 );
 
 // 1) Начальное значение счетчика должно передаваться через props
